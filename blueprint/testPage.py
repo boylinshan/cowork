324fdfab616094a50eb576c09cb4a1b81d477f2e
linshan <boylinshan@163.com>
2017-05-09 20:40:51 +0800

    test ajax

--- blueprint/testPage.py
+++ blueprint/testPage.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 import Globals
-from lib.flask import Blueprint, render_template, abort, request
+from lib.flask import Blueprint, render_template, abort, request, jsonify
 from jinja2 import TemplateNotFound
 from tools.LogManager import LogManager
 
 testPage = Blueprint('test_page', __name__, template_folder='templates')
 
-@testPage.route('/config/<page>/', methods=['GET', 'POST'])
-def show(page):
+@testPage.route('/config/', methods=['GET', 'POST'])
+def show():
 	try:
 		host_group = request.form['host_group']
-		host = request.form['host']
-		action_type = request.form['type']
-		host_list = Globals.cluster[host_group]['hosts'][host]
-		return render_template('pages/config.html', host=host, action_type=action_type, host_list=host_list)
+		host = request.form['host']    
+		info = Globals.cluster[host_group]['hosts'][host]
+		return jsonify(info)
 	except TemplateNotFound:
 		abort(404)
