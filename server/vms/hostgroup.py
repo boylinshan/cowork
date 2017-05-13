0c998770f5b08cf082fd9ed7d74a10f7bd283090
Feiya Song <fsong@splunk.com>
2017-05-13 18:55:56 +0800

    Update parse function.

--- server/vms/hostgroup.py
+++ server/vms/hostgroup.py
@@ -1,4 +1,12 @@
+import os
+import yaml
+from host import Host
+
 
 class HostGroup(object):
-    def __init__(self):
-        pass
+    def __init__(self, filepath):
+        self._group = self._parse_yaml(filepath)
+
+    def _parse_yaml(self, file_path):
+        f = open(file_path)
+        return yaml.load(f)
--- server/vms/vmcluster.py
+++ server/vms/vmcluster.py
@@ -1,11 +1,23 @@
+import os
+from hostgroup import HostGroup
+
+
 class Cluster(object):
     def __init__(self):
         self._host_groups = {}
         self._init_env()
 
     def _init_env(self):
-        pass
+        config_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'configs')
+        for root, dirs, files in os.walk(config_file_path, topdown=False):
+            for f in files:
+                print f
+                if f.endswith('.yml'):
+                    key = f.split(".")[0]
+                    if self._host_groups.has_key(key) is False:
+                        self._host_groups[key] = HostGroup( os.path.join(config_file_path, f) )
 
     @property
     def host_groups(self):
         pass
+
