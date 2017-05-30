ad2992e43fd85c06802ee0eb0c3e3dac988e5e05
linshan <boylinshan@163.com>
2017-05-30 20:46:33 +0800

    update

--- web/module/cluster.py
+++ web/module/cluster.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 import Globals
+import random
 from web.lib.flask import Blueprint, render_template, abort, request, jsonify
 from jinja2 import TemplateNotFound
 
 clusterPage = Blueprint('cluster_page', __name__, template_folder='pages')
+
+cpu_info_list = []
+memory_info_list = []
+disk_info_list = []
+network_info_list = []
+
+def generate(info_list):
+	info_list.append(random.randint(1,100))
+	if len(info_list) > 60:
+		info_list = info_list[len(info_list)-60:]
+
 
 @clusterPage.route('/')
 def cluster():
@@ -17,9 +29,19 @@ def cluster():
 @clusterPage.route('/node', methods=['POST'])
 def node():
 	try:
+		global cpu_info_list, memory_info_list, disk_info_list, network_info_list
+
 		env = request.form['env']
 		node = request.form['node']
-		info = [{'111':12}, {'222':22}]
-		return jsonify(info)
+		generate(cpu_info_list)
+		generate(memory_info_list)
+		generate(disk_info_list)
+		generate(network_info_list)
+		info_dict = {'cpu': cpu_info_list,
+					 'memory': memory_info_list,
+					 'disk': disk_info_list,
+					 'network': network_info_list}
+
+		return jsonify(info_dict)
 	except TemplateNotFound:
 		abort(404)
